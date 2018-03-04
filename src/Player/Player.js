e38cd4bedd98e566266af7e14adeca89a21cdcd3
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-03-04 06:36:17 -0500

    feat(player): add styling for player - finish control bar, rest in-progress

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
 import sampleCover from './sampleCover.jpg';
 
 const Grid = styled.div`
@@ -8,30 +9,52 @@ const Grid = styled.div`
   display: grid;
 
   grid-template-areas:
-    "0-0       showTitle  showTitle  showTitle  showTitle  0-6"
-    "1-0       epTitle    epTitle    epTitle    epTitle    1-6"
+    "0-0       epTitle    epTitle    epTitle    epTitle    0-5"
+    "1-0       showTitle  showTitle  showTitle  showTitle  1-5"
     "previous  cover      cover      cover      cover      next"
-    "previous  seek       seek       seek       seek       next"
-    "rewind    rewind     playPause  playPause  skip       skip";
+    "seek      seek       seek       seek       seek       seek"
+    "controls  controls   controls   controls   controls controls";
 
   grid-gap: 10px;
-  color: #FFF;
-`;
-
-const ShowTitle = styled.div`
-  grid-area: showTitle;
 `;
 
 const EpTitle = styled.div`
   grid-area: epTitle;
+  font-family: "Poppins";
+  font-weight: 600;
+  color: #fff;
+  font-size: 1em;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  height: 10vh;
+  display: flex;
+`;
+
+const ShowTitle = styled.div`
+  grid-area: showTitle;
+  font-family: "Poppins";
+  font-weight: 400;
+  color: #fff;
+  font-size: 1em;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  height: 5vh;
 `;
 
 const Previous = styled.div`
   grid-area: previous;
+  color: #FFF;
+  font-size: 3em;
+  align-self: center;
 `;
 
 const Next = styled.div`
   grid-area: next;
+  color: #fff;
+  font-size: 3em;
+  align-self: center;
 `;
 
 const Cover = styled.div`
@@ -40,37 +63,48 @@ const Cover = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center center;
-  height: 50vh;
+  height: 40vh;
   margin: 0;
   padding: 0;
 `;
 
 const Seek = styled.div`
   grid-area: seek;
+  color: #fff;
 `;
 
-const Rewind = styled.div`
-  grid-area: rewind;
+const Controls = styled.div`
+  grid-area: controls;
+  position: fixed;
+  padding: 1em 0;
+  bottom: 0px;
+  height: 5vh;
+  width: 100vw;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #85cfd8;
+  font-size: 1.56em;
 `;
 
-const Skip = styled.div`
-  grid-area: skip;
-`;
-
-const PlayPause = styled.div`
-  grid-area: playPause;
-`;
+// TODO: find better way to do FontAwesomeIcon styling
+// const styles = {
+//   title: {
+//     'align-self': 'flex-end',
+//   },
+// };
 
 export default function Player() {
   return (
     <Grid>
-      <ShowTitle>
-        The Ezra Klein Show
-      </ShowTitle>
-
       <EpTitle>
         How technology brings out the worst in us, with Tristan Harris
       </EpTitle>
+
+      <ShowTitle>
+        The Ezra Klein Show
+      </ShowTitle>
 
       <Previous>
         <FontAwesomeIcon icon="chevron-left" fixedWidth />
@@ -82,22 +116,16 @@ export default function Player() {
 
       <Cover />
 
-      <Seek>
-        This will be the seek bar
-      </Seek>
+      <Seek>This will be the seek bar</Seek>
 
-      <Rewind>
+      <Controls>
         <FontAwesomeIcon icon="undo" fixedWidth />
-      </Rewind>
 
-      <PlayPause>
         {/* TODO: make play/pause icon variable based on player state */}
         <FontAwesomeIcon icon="play" fixedWidth />
-      </PlayPause>
 
-      <Skip>
         <FontAwesomeIcon icon="redo" fixedWidth />
-      </Skip>
+      </Controls>
 
     </Grid>
   );
