7f30310afe96d32d1cd5a6dd38c7d1aa118580c0
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-03-06 21:13:05 -0500

    test(controls): change implementation of renders test

--- src/Player/Controls/Controls.test.js
+++ src/Player/Controls/Controls.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../../icons';
 
+import '../../icons';
 import Controls from './Controls';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Controls />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Controls', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Controls />, div);
+  });
 });
